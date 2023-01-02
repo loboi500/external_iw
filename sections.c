77d5d62ec59482e166e354eaadd09809e580f621
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-03 02:03:20 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- sections.c
+++ sections.c
@@ -1,4 +1,3 @@
-#include "iw.h"
-
-SECTION(get);
-SECTION(set);
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca528fc7c6edbbafad2770c5dcb12e53c8b9ba83a1f69098fd0b5db5648557b6
+size 45
--- version.sh
+++ version.sh
@@ -1,23 +1,3 @@
-#!/bin/sh
-
-VERSION="4.1"
-OUT="$1"
-
-if [ -d .git ] && head=`git rev-parse --verify HEAD 2>/dev/null`; then
-	git update-index --refresh --unmerged > /dev/null
-	descr=$(git describe --match=v*)
-
-	# on git builds check that the version number above
-	# is correct...
-	[ "${descr%%-*}" = "v$VERSION" ] || exit 2
-
-	v="${descr#v}"
-	if git diff-index --name-only HEAD | read dummy ; then
-		v="$v"-dirty
-	fi
-else
-	v="$VERSION"
-fi
-
-echo '#include "iw.h"' > "$OUT"
-echo "const char iw_version[] = \"$v\";" >> "$OUT"
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4db2444577c545c2c2097e04308e3745b0bc8e104abd796385a03deb1345836
+size 504
